382f0959a53b9c9871c469bb23c966b3a2373c77
Coy Torreblanca <coytorreblanca@Coys-MacBook-Pro.local>
2024-05-12 22:05:33 -0500

    Add child note inheritence and set self from db.

---
+++ bible_notes_md.py
@@ -0,0 +1,111 @@
+# https://bible-notes.atlassian.net/wiki/spaces/~63cdc33695cff7f585c2a3dd/pages/33861/BNMD
+
+import re
+from typing import Optional
+from bible_notes import BibleNote
+from dataclasses import dataclass, field
+
+
+@dataclass
+class BibleNoteMD(BibleNote):
+
+    header_level: int = 0
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        if not self._id:
+            self.extract()
+
+    def extract(self) -> None:
+        """Extract object attributes from note_text."""
+
+        # Split parent from child notes.
+        split_notes = self.note_text.split("#" * self.header_level + " @ ")
+        parent_note = split_notes[0]
+
+        # Check if note is in Mongo database and this note hasn't been updated.
+        if self._set_self_from_db(parent_note):
+            return
+
+        children_notes = self._split_notes[1:]
+
+        # Inherit from child note ids in parent.
+        child_ids_in_parent = re.findall("^@__id([a-z0-9]+)@$", parent_note, flags=re.M)
+        for child_id in child_ids_in_parent:
+            child_note = BibleNote.get(_id=child_id[1:])
+            self._inherit_child_note(child_note=child_note)
+
+        # Add child note ids to parent_note.
+        child_ids_in_parent[0] = "\n" + child_ids_in_parent
+        parent_note += "\n".join(child_ids_in_parent)
+
+        # if not _id:
+        # raise ValueError("Note text did not contain a valid _id field.")
+
+    @classmethod
+    def _split_notes(cls, note_text: str, header_level_of_parent: int) -> list[str]:
+        """Split notes into a list containing text of parent note and immediate sub-notes.
+
+        Args:
+            note_text (str): The text to split be headers.
+            header_level_of_parent (int): The header level of the parent.
+
+        Returns:
+            list[str]: A list containing text of parent note and immediate sub-notes.
+        """
+
+        header_of_immediate_child_notes = "#" * (header_level_of_parent + 1) + " @ "
+        split_notes = re.split(rf"\n{header_of_immediate_child_notes}", note_text)
+
+        # Add headers back and strip.
+        split_notes[0] = split_notes[0].strip()
+        for i in range(1, len(split_notes)):
+            split_notes[i] = (
+                f"{ header_of_immediate_child_notes }{ split_notes[i].strip() }"
+            )
+
+        return split_notes
+
+    def _set_self_from_db(self, parent_note_text: str) -> bool:
+        """Set attributes from mongodb if text matches note in mongodb.
+
+        Args:
+            parent_note_text (str): The note of most parent text without child note text.
+
+
+        Returns:
+            bool: Whether self attributes could be set from mongodb.
+        """
+
+        if not self._id:
+            _id = re.search("^@_id([a-z0-9]+)@$", parent_note_text, flags=re.M)
+
+            bible_note = BibleNoteMD.get(_id=_id)
+
+            # Set this object equal to the object in the database for simplicity.
+            if bible_note.note_text == self.note_text:
+                for attribute, value in bible_note.to_db_dict().items():
+                    self.__setattr__(attribute, value)
+                return True
+
+            return False
+
+    def _inherit_child_note(self, child_note: "BibleNoteMD") -> None:
+        """Inherit attributes from the provided child note to this object.
+        NOTE - Does not modify self.note_text.
+
+        Args:
+            child_note (BibleNoteMD): Child note to inherit from.
+        """
+        # Add child tags to parent.
+        for key, value in child_note.key_value_tags.items():
+            if key not in self.tags:
+                self.key_value_tags[key] = value
+
+        self.tags.update(child_note.tags)
+
+        # Add child verse/note references to parent.
+        self.referenced_verses.update(child_note.referenced_verses)
+        self.referenced_notes.update(child_note.referenced_notes)
+        self.referenced_notes.add(child_note._id)
--- test_bible_notes_md.py
+++ test_bible_notes_md.py
@@ -1,4 +1,6 @@
 import unittest
+from unittest.mock import patch
+from datetime import datetime
 from bible_notes_md import BibleNoteMD
 
 h0 = """asdlkfja;sdf
@@ -106,7 +108,7 @@ test_note = f"{h0}\n{h1}\n{h2}\n{h1}\n{h2_2}\n{h3}\n{h2}"
 class TestBibleNotesMD(unittest.TestCase):
     def test_note_split(self):
         # Test level one split.
-        note_split_level_1 = BibleNoteMD.split_notes(test_note, 0)
+        note_split_level_1 = BibleNoteMD._split_notes(test_note, 0)
 
         self.assertEqual(note_split_level_1[0], h0)
 
@@ -115,27 +117,145 @@ class TestBibleNotesMD(unittest.TestCase):
         self.assertEqual(note_split_level_1[2], f"{h1}\n{h2_2}\n{h3}\n{h2}".strip())
 
         # Test level two splits.
-        note_split_level_2 = BibleNoteMD.split_notes(note_split_level_1[1], 1)
+        note_split_level_2 = BibleNoteMD._split_notes(note_split_level_1[1], 1)
 
         self.assertEqual(note_split_level_2[0], h1.strip())
         self.assertEqual(note_split_level_2[1], h2.strip())
 
-        note_split_level_2 = BibleNoteMD.split_notes(note_split_level_1[2], 1)
+        note_split_level_2 = BibleNoteMD._split_notes(note_split_level_1[2], 1)
 
         self.assertEqual(note_split_level_2[0], h1.strip())
         self.assertEqual(note_split_level_2[1], f"{h2_2}\n{h3}".strip())
         self.assertEqual(note_split_level_2[2], f"{h2}".strip())
 
         # Test level three splits
-        note_split_level_3 = BibleNoteMD.split_notes(note_split_level_2[1], 2)
+        note_split_level_3 = BibleNoteMD._split_notes(note_split_level_2[1], 2)
 
         self.assertEqual(note_split_level_3[0], h2_2.strip())
         self.assertEqual(note_split_level_3[1], h3.strip())
 
-        note_split_level_3 = BibleNoteMD.split_notes(note_split_level_2[2], 2)
+        note_split_level_3 = BibleNoteMD._split_notes(note_split_level_2[2], 2)
 
         self.assertEqual(note_split_level_3[0], h2.strip())
         self.assertTrue(len(note_split_level_3) == 1)
+
+    def test_child_inheritence(self):
+
+        # Set uup parent/child verses.
+        parent_kv_tags = {"tagged": False, "existing_tag": "here"}
+        parent_tags = {"one", "three"}
+        parent_referenced_verses = {"/John/1/1", "/John/1/2"}
+
+        bible_note_parent = BibleNoteMD(
+            _id="_idabc",
+            note_text="this is normal text.",
+            theme="this is a . normal theme",
+            key_value_tags=parent_kv_tags,
+            tags=parent_tags,
+            date_created=datetime.now(),
+            date_updated=datetime.now(),
+            referenced_verses=parent_referenced_verses,
+        )
+
+        child_id = "_id1234545"
+        child_kv_tags = {"tagged": True, "new_tag": "is_here"}
+        child_tags = {"one", "two"}
+        child_referenced_notes = {"_id9108103"}
+        child_referenced_verses = {"/John/1/1", "/John/1/2"}
+
+        bible_note_child = BibleNoteMD(
+            _id=child_id,
+            note_text="asfj2093",
+            theme="28032j",
+            key_value_tags=child_kv_tags,
+            tags=child_tags,
+            date_created=datetime.now(),
+            date_updated=datetime.now(),
+            referenced_notes=child_referenced_notes,
+            referenced_verses=child_referenced_verses,
+        )
+
+        # Copy objects to test for attributes which shouldn't change.
+        bible_note_child_dict = bible_note_child.to_db_dict()
+        parent_note_dict = bible_note_parent.to_db_dict()
+
+        # Simulate updated values to test.
+        updated_parent_kv_tags = parent_kv_tags
+        for key, value in child_kv_tags.items():
+            if key not in parent_kv_tags:
+                updated_parent_kv_tags[key] = value
+
+        updated_parent_tags = parent_tags.copy()
+        updated_parent_tags.update(child_tags)
+
+        updated_referenced_notes = child_referenced_notes.copy()
+        updated_referenced_notes.add(child_id)
+
+        updated_referenced_verses = child_referenced_verses
+
+        # Inherit child in parent.
+        bible_note_parent._inherit_child_note(bible_note_child)
+
+        # Ensure parent inherited values are correct.
+        self.assertEqual(bible_note_parent.key_value_tags, updated_parent_kv_tags)
+        self.assertEqual(bible_note_parent.tags, updated_parent_tags)
+        self.assertEqual(bible_note_parent.referenced_verses, updated_referenced_verses)
+        self.assertEqual(bible_note_parent.referenced_notes, updated_referenced_notes)
+
+        # Ensure child is unchanged.
+        self.assertEqual(bible_note_child.to_db_dict(), bible_note_child_dict)
+
+        # Ensure other parent values are unchanged.
+        new_parent_dict = bible_note_parent.to_db_dict()
+        del new_parent_dict["tags"]
+        del new_parent_dict["key_value_tags"]
+        del new_parent_dict["referenced_notes"]
+        del new_parent_dict["referenced_verses"]
+        del parent_note_dict["referenced_verses"]
+        del parent_note_dict["referenced_notes"]
+        del parent_note_dict["tags"]
+        del parent_note_dict["key_value_tags"]
+        self.assertEqual(new_parent_dict, parent_note_dict)
+
+    @patch("bible_notes_md.BibleNoteMD.get")
+    def test_set_self_from_db(self, mock_get_bible_note):
+        # Create preliminary data required
+        note_id = "12234509147018390"
+
+        note_text = f"@_id{note_id}@\n" + test_note[:]
+
+        note_in_mongo = BibleNoteMD(
+            _id=note_id,
+            note_text=note_text,
+            referenced_notes={"1901830113", "910930909"},
+            referenced_verses={"/John1/1/1", "/Genesis/1/1"},
+            tags={"tagged"},
+            key_value_tags={"tagged": True},
+        )
+
+        mock_get_bible_note.return_value = note_in_mongo
+
+        note_in_mongo_dict = note_in_mongo.to_db_dict()
+
+        parent_note = BibleNoteMD._split_notes(
+            note_text=note_text, header_level_of_parent=0
+        )[0]
+
+        # Pass id, then remove it, to bypass extraction.
+        new_bible_note = BibleNoteMD(_id="190190", note_text=note_text)
+        new_bible_note._id = None
+
+        # Set attritubes of new bible note.
+        new_bible_note._set_self_from_db(parent_note)
+        self.assertEqual(note_in_mongo_dict, new_bible_note.to_db_dict())
+
+        # Test that self is not replaced if note text is different.
+        # Pass id, then remove it, to bypass extraction.
+        new_bible_note = BibleNoteMD(_id="190190", note_text=note_text + ".")
+        new_bible_note._id = None
+
+        new_bible_note._set_self_from_db(parent_note)
+        self.assertNotEqual(note_in_mongo_dict, new_bible_note.to_db_dict())
 
 
 if __name__ == "__main__":
