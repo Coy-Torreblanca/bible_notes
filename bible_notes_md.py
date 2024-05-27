9f92430fa7cc4726e8ac5dd0acf08ae0dda3bf1c
Coy Torreblanca <coytorreblanca@Coys-MacBook-Pro.local>
2024-05-27 12:24:24 -0500

    init extract.

--- bible_notes_md.py
+++ bible_notes_md.py
@@ -1,10 +1,11 @@
 # https://bible-notes.atlassian.net/wiki/spaces/~63cdc33695cff7f585c2a3dd/pages/33861/BNMD
+# TODO - child first search
 
 import re
 import uuid
 from typing import Optional
 from bible_notes import BibleNote
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 CHILD_ID_REGEX = "^@__id([a-z0-9]+)@$"
 _ID_REGEX = "^@_id([a-z0-9]+)@$"
@@ -34,7 +35,47 @@ class BibleNoteMD(BibleNote):
 
         # Split parent from child notes.
         split_notes = self._split_notes(self.note_text, self.header_level)
+
+        # Extract child notes.
+        children_notes = []
+        child_ids = []
+
+        children_have_been_modified = False
+
+        # Process child notes.
+        for i in range(1, len(split_notes)):
+            child_note = BibleNoteMD(
+                note_text=split_notes[i], header_level=self.header_level + 1
+            )
+
+            children_notes.append(child_note)
+
+            child_has_been_modified = child_note.extract()
+
+            child_ids.append(child_note._id)
+
+            if child_has_been_modified or child_note._id not in self.referenced_notes:
+                # Child has been modified or never inherited.
+                # Mark flag to ensure we inherit child notes.
+                children_have_been_modified = True
+
+        if children_have_been_modified:
+            # Reset properties of parent and inherit children.
+            self.key_value_tags = {}
+            self.tags = set()
+            self.referenced_verses = set()
+            self.referenced_verses = set()
+
+            for child in children_notes:
+                self._inherit_child_note(child)
+
         parent_text = split_notes[0]
+
+        # Contract note text for efficiency and to meet `set_self_from_db` requirements.
+        # Contracted note text is parent text with all children note ids appended.
+
+        for child_id in child_ids:
+
 
         if not self._id:
             _id = re.search(_ID_REGEX, parent_text, flags=re.M)
@@ -45,14 +86,20 @@ class BibleNoteMD(BibleNote):
             else:
                 _id = _id.group(1)
 
+                if not children_have_been_modified:
+                    has_been_modified = self.set_self_from_db(check_note_text=True)
+
+                else:
+                    has_been_modified = True
+
             self._id = _id
 
-        self._extract_attr_from_parent_text(parent_text=parent_text)
+        if has_been_modified:
+            # TODO Update Mongo.
+            self._extract_attr_from_parent_text(parent_text=parent_text)
 
-        # TODO Move to split note area as you will need to add text by conca
-        # Extract/Create _id.
-
-        children_notes = self._split_notes[1:]
+        # If children have been modified or self has been modified, return True.
+        return has_been_modified
 
         # if not _id:
         # raise ValueError("Note text did not contain a valid _id field.")
@@ -148,9 +195,11 @@ class BibleNoteMD(BibleNote):
                 # This is a kv tag with a null key.
                 self.tags.add(split_tag[0])
 
-    def set_self_from_db(self) -> bool:
+    def set_self_from_db(self, check_note_text: bool = False) -> bool:
         """Set attributes from mongodb if text matches note in mongodb.
         Extract and set _id field.
+
+        NOTE: Assumes self.note_text has been contracted if check_note_text is True.
 
         Returns:
             bool: Whether self attributes could be set from mongodb.
@@ -162,6 +211,8 @@ class BibleNoteMD(BibleNote):
 
         # Set this object equal to the object in the database for simplicity.
         if bible_note:
+            if check_note_text and self.note_text != bible_note.note_text:
+                return False
             for attribute, value in bible_note.to_db_dict().items():
                 self.__setattr__(attribute, value)
             return True
@@ -186,6 +237,7 @@ class BibleNoteMD(BibleNote):
     def _inherit_child_note(self, child_note: "BibleNoteMD") -> None:
         """Inherit attributes from the provided child note to this object.
         NOTE - Does not modify self.note_text.
+        NOTE - Only location where referenced_notes should be modified.
 
         Args:
             child_note (BibleNoteMD): Child note to inherit from.
